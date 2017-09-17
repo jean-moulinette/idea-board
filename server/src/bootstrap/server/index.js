38379c71d878605cbc0d7b12ab87fe30ff15385b
Fanghornn <fanghornn@gmail.com>
2017-09-17 18:13:58 +0200

    jsconfig updated

--- server/src/bootstrap/server/index.js
+++ server/src/bootstrap/server/index.js
@@ -1,6 +1,8 @@
-const { serverFactory } = require('src/bootstrap/server/utils/serverFactory.js')
+const { serverFactory } = require('src/bootstrap/server/utils/serverFactory')
 
 exports.bootstrapServer = () => {
   const server = serverFactory()
   server.startServer()
+
+  return server
 }
