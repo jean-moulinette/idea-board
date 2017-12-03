347e8d7ddd9e25dd4c955fb81a506d3bae0680e8
fangornn <fanghornn@gmail.com>
2017-12-03 17:49:22 +0100

    dot env initialization in integration testing

--- server/tests/index.spec.js
+++ server/tests/index.spec.js
@@ -1,4 +1,6 @@
 /* eslint-disable global-require */
+require('dotenv').config()
+
 before(async () => {
   // eslint-disable-next-line no-undef
   app = require('src/bootstrap/server').ideaBoard.startServer()
