83f628ae0ec2c9d0fe94750e16e1d568ee3f7562
fangornn <fanghornn@gmail.com>
2017-12-17 20:54:54 +0100

    Adds a global logger for unhandled reject promises

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 const { ideaBoard } = require('src/bootstrap/server')
 
 function main() {
+  // Global error handler for uncatched rejected promises
+  if (process.env.NODE_ENV !== 'production') {
+    process.on('unhandledRejection', (reason, rejectedPromise) => {
+      const errorMsg = 'Unhandled promise rejection catched in server process'
+      console.log(`\n${errorMsg}\n Promise: `, rejectedPromise, 'Reason: ', reason)
+    })
+  }
+
   ideaBoard.startServer()
 }
 
--- server/tests/index.spec.js
+++ server/tests/index.spec.js
@@ -1,6 +1,11 @@
 /* eslint-disable global-require */
 require('dotenv').config()
 
+process.on('unhandledRejection', (_, rejectedPromise) => {
+  const errorMsg = 'Promise rejection catched in server\'s testing process'
+  console.log(`\n${errorMsg}\n Promise: `, rejectedPromise)
+})
+
 before(async () => {
   // eslint-disable-next-line no-undef
   app = require('src/bootstrap/server').ideaBoard.startServer()
